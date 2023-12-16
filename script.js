34a5c373b27150d2b5538d6ff4e9bd7fa61107d3
MrChrisFabian <christopher.mendoza@fiuni.edu.py>
2023-12-16 01:05:59 -0300

    streak's system created

---
+++ script.js
@@ -0,0 +1,62 @@
+
+const BOTON = document.getElementById('boton');
+const INBOTON = document.getElementById('inicioBoton');
+const TEXT = document.getElementById('msgInicio');
+localStorage.setItem('ultimo',JSON.stringify({dia:0,mes:0}));
+INBOTON.addEventListener('click', () => {
+    const INICIORACHA = inicioRacha();
+    localStorage.setItem('inicio', JSON.stringify({ dia: INICIORACHA[0], mes: INICIORACHA[1] }));
+    localStorage.setItem('racha', 0);
+})
+
+BOTON.addEventListener('click', () => {
+    if(compareUwI()){
+        if (checkRacha) {
+            sumarRacha();
+            ultimaRacha();
+        }
+    }
+})
+
+function inicioRacha() {
+    const INICIORACHA = fechaActual();
+    return INICIORACHA;
+}
+
+function checkRacha() {
+    let esteDia = fechaActual();
+    let inicio = JSON.parse(localStorage.getItem('inicio'))
+    if ((esteDia[0] - inicio.dia) == 1) {
+        console.log('true sale de aca')
+        return true;
+    }
+    else {
+        if (esteDia[1] != inicio.mes) {
+            console.log('true sale de mes')
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+function fechaActual() {
+    let fechaDeHoy = new Date();
+    const INICIORACHA = [fechaDeHoy.getDate(), fechaDeHoy.getMonth() + 1];
+    return INICIORACHA;
+}
+function sumarRacha() {
+    let rachaActual = parseInt(localStorage.getItem('racha')) || 0;
+    let rachaActualizada = rachaActual + 1;
+    localStorage.setItem('racha', rachaActualizada);
+}
+function ultimaRacha(){
+    let ultimoDia = fechaActual();
+    localStorage.setItem('ultimo', JSON.stringify({ dia: ultimoDia[0], mes: ultimoDia[1] }));    
+}
+function compareUwI(){
+    let ultDia = JSON.parse(localStorage.getItem('ultimo'));
+    let actualF = fechaActual();
+    return(ultDia.dia != actualF[0]);
+}
+
